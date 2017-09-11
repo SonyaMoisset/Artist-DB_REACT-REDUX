ec87e54d9ff3666405f50da30d9ac8600facf6b5
smoisset <smoisset@worldremit.com>
2017-09-11 22:19:27 +0100

    Add map to artists list

--- src/components/artists_list.js
+++ src/components/artists_list.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ArtistsList = (props) => {
-    console.log(props)
+    
+    const list = ({ allArtists }) => {
+        if (allArtists) {
+            return allArtists.map((item) => {
+                const style = {
+                    background: `url('/images/covers/${item.cover}.jpg') no-repeat`
+                }
+                return (
+                    <Link
+                        key={item.id}
+                        to={`/artist/${item.id}`}
+                        className="artist_item"
+                        style={style}>
+                        <div>{item.name}</div>
+                    </Link>
+                )
+            })
+        }
+    }
+
     return (
-        <div>List</div>
+        <div className="artists_lists">
+            <h4>Browse the artists</h4>
+            {list(props)}
+        </div>
     )
 }
 
