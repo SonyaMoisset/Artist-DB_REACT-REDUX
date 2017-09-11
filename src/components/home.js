27f1d4f56027ee67695e0a7d4236254049c5f2a6
smoisset <smoisset@worldremit.com>
2017-09-11 22:00:45 +0100

    Add banner for homepage

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Banner from './banner'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Banner />
+
+
+            </div>
+        )
+    }
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './components/home'
+import Artist from './components/artist'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/artist/:artistid" component={Artist} />
+            </div>    
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
