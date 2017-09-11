84086b3a616c0f9cf4e4ce083a5d07b15898f359
smoisset <smoisset@worldremit.com>
2017-09-11 22:10:04 +0100

    Add artists list component

---
+++ src/components/artists_list.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ArtistsList = (props) => {
+    console.log(props)
+    return (
+        <div>List</div>
+    )
+}
+
+export default ArtistsList
--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 
 import Banner from './banner'
+import ArtistsList from './artists_list'
+
+const URL_ARTISTS = 'http://localhost:3004/artists'
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            artists: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_ARTISTS, {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                artists: json
+            })
+        })
+    }
+
     render() {
         return (
             <div>
                 <Banner />
-
-
+                <ArtistsList allArtists={this.state.artists} />
             </div>
         )
     }
